0b361d3f8eb0932efeab17df9a8ca5238300b0bd
Benjamin Nosarzewski <bln@rhel6-64g.slac.stanford.edu>
2017-09-01 17:31:51 -0700

    mu map for nvs mu for omegas 2p8 and 3p6

--- make_mu_map_Holstein8b8_simes.py
+++ make_mu_map_Holstein8b8_simes.py
@@ -4,13 +4,25 @@ N = 64
 W = 8.
 
 
-folder = 5
+folder = 8
 
 # new set of omegas
 if folder==5:
     omegas = [2.8, 3.6]
     betas  = [0.8, 1.6, 2.4, 4.8, 8.0, 12.0, 16.0]
     lamb   = 0.3
+if folder==6:
+    omegas = [2.8, 3.6]
+    betas  = [0.8, 1.6, 2.4, 4.8, 8.0, 12.0, 16.0]
+    lamb   = 0.4
+if folder==7:
+    omegas = [2.8, 3.6]
+    betas  = [0.8, 1.6, 2.4, 4.8, 8.0, 12.0, 16.0]
+    lamb   = 0.5
+if folder==8:
+    omegas = [2.8, 3.6]
+    betas  = [0.8, 1.6, 2.4, 4.8, 8.0, 12.0, 16.0]
+    lamb   = 0.6
 
 
     
@@ -22,6 +34,12 @@ save('../N',N)
 
 if folder==5:
     mu_map = load('../mu_map_interpolated_nvsmu_l0p3_8b8.npy')
+if folder==6:
+    mu_map = load('../mu_map_interpolated_nvsmu_l0p4_8b8.npy')
+if folder==7:
+    mu_map = load('../mu_map_interpolated_nvsmu_l0p5_8b8.npy')
+if folder==8:
+    mu_map = load('../mu_map_interpolated_nvsmu_l0p6_8b8.npy')
     
     
 save('../mu_map', mu_map)
